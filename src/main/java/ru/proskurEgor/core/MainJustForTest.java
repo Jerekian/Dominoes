7b1ce4032b652f94e497e79f806117f59699bd64
Jerekian <proskur.egor@yandex.ru>
2019-09-20 23:10:24 +0300

    Проверка новых функций

--- src/main/java/ru/proskurEgor/core/MainJustForTest.java
+++ src/main/java/ru/proskurEgor/core/MainJustForTest.java
@@ -1,16 +1,24 @@
 package ru.proskurEgor.core;
 
+import ru.proskurEgor.data.OrderedBones;
+import ru.proskurEgor.utils.DominoesUtils;
+
 public class MainJustForTest {
 
     public static void main(String[] args) {
 
         Market market = new Market();
 
+        System.out.println(DominoesUtils.listToString(OrderedBones.getAllBones()));
         System.out.println(market.toString());
         market.randomSort();
         System.out.println(market.toString());
-        market.ascendingSort();
-        System.out.println(market.toString());
+        System.out.println("----------");
+        for(int i = market.size(); i > 0; i = market.size()){
+            System.out.println(market.getBone() + "  " + market.size());
+        }
+
+
     }
 
 
